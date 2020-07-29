9eae310a1b05410b36a12c9c863770d6caa9e904
Adam Palmer <theadampalmer@gmail.com>
2020-07-29 14:56:30 -0500

    update

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,10 @@
-#where we set up what app requires
+# where we set up what app requires
 require 'pry'
 require 'httparty'
 require 'nokogiri'
+
+require_relative '../lib/cli.rb'
+require_relative '../lib/api.rb'
+require_relative '../lib/office_quotes.rb'
+#one dot means look in current folder, two dots means look where i'm at and up one from there
+
