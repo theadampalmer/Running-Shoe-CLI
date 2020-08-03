52c4237bc0097abcac44eaa572a4811c1694d958
Adam Palmer <theadampalmer@gmail.com>
2020-08-02 22:51:28 -0500

    updated CLI

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,12 @@
 class Scraper
 
     def scraping_tool
-	    url = "https://www.scarymommy.com/michael-scott-quotes/"
+url = "https://www.scarymommy.com/michael-scott-quotes/"
 	    unparsed_page = HTTParty.get(url)
 	    parsed_page = Nokogiri::HTML(unparsed_page)
 	    byebug
     end
 
-end
 
 
 #no puts or gets in here at all
